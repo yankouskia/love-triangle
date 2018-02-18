5afdd410977c22f90af5f9d202f34876cbe1b910
yankouskia <aleksandr.yankovskiy@gmail.com>
2018-02-18 20:05:37 +0300

    😍

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+  // your implementation
+};
