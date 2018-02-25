4f78a5b35832420a1b8424626ea56fe4c8d1ee0e
Nov3m63r <no.v3m63r@gmail.com>
2018-02-25 16:50:57 +0300

    Love-triangle implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(preferences) {
+  // my implementation
+  var sp1 = 0;
+  var sp2 = 0;
+  var sp3 = 0;
+  var triangle = 0;
+  for (var i=0;i<preferences.length;i++) {
+  	sp1 = i+1;
+  	sp2 = preferences[i];
+  	sp3 = preferences[sp2-1];
+ 		if (preferences[sp3-1] == sp1 && sp1 != sp2 && sp2 != sp3) {
+  			triangle += 1;
+  			}
+  		}
+  return triangle/3;
+}
