25ec0b2cf71222cde3bf5eb8172f1df8de0461e8
Viktar Zhurauski <43149882+Kottt4@users.noreply.github.com>
2018-09-12 22:01:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+var count =0;
+// a+1, b+1, c+1 -порядковый номер мурзилки
+for (var a =0; a < preferences.length; a++) {
+  for (var b =0; b < preferences.length; b++) {
+   if (b != a && b+1 === preferences[a]) {
+    for (var c =0; c < preferences.length; c++) {
+     if (c !=a && c !=b && c+1 ===preferences[b] && a+1 ===preferences[c]) {
+      count ++;
+     };
+    };
+   };
+  };
+};
+count = count/3;
+return count;
 };
